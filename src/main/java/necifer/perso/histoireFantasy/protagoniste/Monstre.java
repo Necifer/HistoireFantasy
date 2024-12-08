ade4a2ef35d7be199c6910927144321c9cf28aef
Necifer <combyguillaume@gmail.com>
2024-12-08 17:07:26 +0100

    Finalisation partie 1 : Classe monstre terminée

--- src/main/java/necifer/perso/histoireFantasy/protagoniste/Monstre.java
+++ src/main/java/necifer/perso/histoireFantasy/protagoniste/Monstre.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 import lombok.ToString;
 import necifer.perso.histoireFantasy.attaque.Pouvoir;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+
 @Getter
 @ToString
 public class Monstre extends EtreVivant {
@@ -12,22 +16,65 @@ public class Monstre extends EtreVivant {
     private final ZoneDeCombat zoneDeCombat;
     private final Domaine domaine;
 
-    private Pouvoir[] attaques;
+    private final Pouvoir[] attaques;
+    private GestionAttaque gestionAttaque;
 
     public Monstre(String nom, int forceDeVie, ZoneDeCombat zoneDeCombat, Domaine domaine, Pouvoir... attaques) {
 
         super(nom, forceDeVie);
         this.domaine = domaine;
         this.zoneDeCombat = zoneDeCombat;
-        this.attaques = getAttaques();
+        this.attaques = attaques;
     }
 
-    public void attaque() {
-        if (GestionAttaque.hasNext() = true) {
+    public void entreEnCombat() {
 
-            return super.Pouvoir.utiliser();
+        Arrays.stream(attaques).forEach(Pouvoir::regenererPouvoir);
+        /* equivalent de la boucle for each ci dessous
+        for (Pouvoir pouvoir : attaques) {
+            pouvoir.regenererPouvoir();
+        }*/
+        gestionAttaque = new GestionAttaque();
+    }
+
+    public Pouvoir attaque() {
+
+
+        return gestionAttaque.hasNext() ? gestionAttaque.next() : null; // operateur ternaire : "condition a evaluer" ? "comportement si true" : "comportement si false"
+        /* equivalent du if ci dessous :
+
+        if (gestionAttaque.hasNext()) {
+            return gestionAttaque.next();
         } else {
-            return ("null");
+            return null;
+        }*/
+    }
+
+    private class GestionAttaque implements Iterator<Pouvoir> {
+
+        private final Pouvoir[] attaquesPossibles = attaques;
+        private int nbAttaquesPossibles = attaques.length;
+
+        @Override
+        public boolean hasNext() {
+
+            for (int i = 0; i < nbAttaquesPossibles; i++) {
+
+                if (!attaquesPossibles[i].isOperationnel()) {
+
+                    attaquesPossibles[i] = attaquesPossibles[nbAttaquesPossibles - 1];
+                    nbAttaquesPossibles--;
+                }
+            }
+            return nbAttaquesPossibles > 0;
+        }
+
+        @Override
+        public Pouvoir next() {
+
+            return attaquesPossibles[new Random().nextInt(nbAttaquesPossibles)];
         }
     }
+
+
 }
